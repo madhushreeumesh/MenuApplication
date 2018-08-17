cce4fcc782a9bedd4191cdfa82198093ce6cdad8
madhushree <madhushreeumesh1995@gmail.com>
2018-08-17 17:02:28 +0530

    commit

--- app/src/main/java/com/example/tapanjoshi/menuapplication/MainActivity.java
+++ app/src/main/java/com/example/tapanjoshi/menuapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tapanjoshi.menuapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv1=(TextView)findViewById(R.id.tv_getstarted);
+        TextView tv2=(TextView)findViewById(R.id.tv_getaccount);
+
+        tv1.setOnClickListener(new View.OnClickListener()
+        {   public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        tv2.setOnClickListener(new View.OnClickListener()
+        {   public void onClick(View v)
+        {
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+        });
     }
 
         @Override
@@ -31,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
             {
                 case R.id.menu_save:
                     Toast.makeText(MainActivity.this, "Save is Selected", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
                     return true;
 
                 case R.id.menu_search:
@@ -39,10 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
                 case R.id.menu_share:
                     Toast.makeText(MainActivity.this, "Share is Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-
-                case R.id.menu_delete:
-                    Toast.makeText(MainActivity.this, "Delete is Selected", Toast.LENGTH_SHORT).show();
                     return true;
 
                 default:
