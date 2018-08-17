bf2bf9da93ebeac55b99a8c507ff311b6f9a1253
madhushree <madhushreeumesh1995@gmail.com>
2018-08-17 14:04:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tapanjoshi/menuapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tapanjoshi.menuapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            MenuInflater menuInflater = getMenuInflater();
+            menuInflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
+
+            switch (item.getItemId())
+            {
+                case R.id.menu_save:
+                    Toast.makeText(MainActivity.this, "Save is Selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                case R.id.menu_search:
+                    Toast.makeText(MainActivity.this, "Search is Selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                case R.id.menu_share:
+                    Toast.makeText(MainActivity.this, "Share is Selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                case R.id.menu_delete:
+                    Toast.makeText(MainActivity.this, "Delete is Selected", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
+    }
+
+
